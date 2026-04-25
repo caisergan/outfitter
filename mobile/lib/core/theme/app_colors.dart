2d7be01bd5386b4ce3ba08e1f4287f416f43086c
wiyuuwiyuu <ayseserraer@gmail.com>
2026-04-25 16:00:28 +0300

    feat: new colors
    I added a new color palette

--- mobile/lib/core/theme/app_colors.dart
+++ mobile/lib/core/theme/app_colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const mint = Color(0xFFC0E1D2);
-  static const lightMint = Color(0xFFE5EEE4);
-  static const cream = Color(0xFFF6F4E8);
-  static const blush = Color(0xFFDC9B9B);
-  static const text = Color(0xFF452829);
+  static const mint = Color(0xFF7AAACE);
+  static const lightMint = Color(0xFFbbe2ff);
+  static const cream = Color(0xFFF7F8F0);
+  static const blush = Color(0xFF355872);
+  static const text = Color(0xFF281C59);
 }
